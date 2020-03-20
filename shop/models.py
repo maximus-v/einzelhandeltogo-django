a646783f60acd048d6717e10ce76e050d4a16edd
Christoph <christoph.sager@tu-dresden.de>
2020-03-21 00:47:30 +0100

    Created model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Driver(models.Model):
+    STATUS = (
+        ('A', 'Available'),
+        ('U', 'Unavailable'),
+        ('I', 'Idle'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phonenumber = models.CharField(max_length=20)
+    gps_position = models.CharField(max_length=100)
+    status = models.CharField(max_length=1, choices=STATUS)
+
+class Buyer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phonenumber = models.CharField(max_length=20)
+    address = models.CharField(max_length=300)
+    email = models.EmailField(max_length=254)
+
+class Seller(models.Model):
+    STATUS = (
+        ('O', 'Open'),
+        ('C', 'Closed'),
+        ('I', 'Idle'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=300)
+    shop_category = models.CharField(max_length=300)
+    products = models.CharField(max_length=600)
+    phonenumber = models.CharField(max_length=20)
+    address = models.CharField(max_length=300)
+    email = models.EmailField(max_length=254)
+    status = models.CharField(max_length=1, choices=STATUS)
+
+class Transaction(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    loaded = models.DateTimeField()
+    delivered = models.DateTimeField()
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
+    buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE)
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    product = models.CharField(max_length=600)

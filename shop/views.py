cd03ad3bea1d32cbae79f657cf0b786125f24195
Max Vorhauer <max.vorhauer@tu-dresden.de>
2020-03-21 09:37:01 +0100

    added todos for needed endpoints

--- shop/views.py
+++ shop/views.py
@@ -6,6 +6,32 @@ from shop.models import Seller, Buyer, Driver
 from shop.serializers import UserSerializer, SellerSerializer, BuyerSerializer, DriverSerializer
 
 
+# Following views / endpoints must be implemented
+# TODO GET list of buyers
+# TODO GET buyer details
+# TODO POST register as buyer
+# TODO UPDATE buyer profile
+# TODO DELETE buyer profile
+# TODO GET list of sellers
+# TODO GET seller details
+# TODO POST register as seller
+# TODO UPDATE seller profile
+# TODO DELETE seller profile
+# TODO GET list of drivers
+# TODO GET driver details
+# TODO POST register as driver
+# TODO UPDATE driver profile
+# TODO DELETE driver profile
+# TODO GET list of driver's transactions
+# TODO GET list of buyer's transactions
+# TODO GET list of seller's transactions
+# TODO GET transaction details
+# TODO POST new transaction
+# TODO UPDATE existing transaction
+# TODO DELETE transaction
+# TODO POST login with credentials
+
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
